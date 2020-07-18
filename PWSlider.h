f1d3cbc6aee8ec453132ee41c066489bcf9a6b5d
Manuel Cantu <m.cantu.reinhard@gmail.com>
2020-07-18 21:33:17 +0200

    Add example, missing destructor in slider h file

--- PWSlider.h
+++ PWSlider.h
@@ -11,6 +11,7 @@ class PWSlider: public PrimitiveWidget {
     void updateSlider(uint16_t posx, uint16_t color);
     public:
     PWSlider(uint16_t colorb, uint16_t colorbp, uint16_t tcolor, uint16_t x, uint16_t y, uint16_t w, uint16_t h, GFX4d *gfx, void (*callback)(int), uint8_t initial_pos);
+    ~PWSlider();
     void touched(uint16_t x, uint16_t y);
     void released();
 };
---
+++ example/example.ino
@@ -0,0 +1,101 @@
+/*
+    Manuel Cantu - GFX4dPrimitiveWidgets example file
+*/
+
+#include "GFX4d.h"
+#include "PWButton.h"
+#include "PWSlider.h"
+#include "LedDigit.h"
+
+GFX4d gfx = GFX4d();
+
+#define btny 0
+#define btnh 60
+#define sldy 70
+#define sldh 60
+#define sld2y 140
+#define sld2h 40
+
+void cbtest(int val){
+  Serial.print("cbtest: ");
+  Serial.println(val);
+}
+
+int m = 0;
+int c = 0;
+int d = 0;
+int u = 0;
+PWSlider *sldr;
+PWSlider *sldrtwo;
+PWButton *btn;
+bool btnP = false;
+LedDigit *mill;
+LedDigit *cent;
+LedDigit *dec;
+LedDigit *uni;
+
+void setup() {
+  Serial.begin(115200);
+  gfx.begin();
+  gfx.Cls();
+  gfx.ScrollEnable(true);
+  gfx.BacklightOn(true);
+  gfx.Orientation(PORTRAIT);
+  gfx.SmoothScrollSpeed(5);
+  gfx.TextColor(WHITE); gfx.Font(2);  gfx.TextSize(1);
+  gfx.touch_Set(TOUCH_ENABLE);
+  
+  btn = new PWButton(BLUE, RED, WHITE, 0,btny,240,btnh, "Im a button", &gfx, &cbtest, 2);
+
+  sldr = new PWSlider(GREEN, RED, WHITE, 0,sldy,240,sldh, &gfx, &cbtest, 0);
+  sldrtwo = new PWSlider(CHOCOLATE, RED, AQUA, 0,sld2y,120,sld2h, &gfx, &cbtest, 0);
+  
+  mill = new LedDigit(0, 190, BLACK, LIME, &gfx);
+  cent = new LedDigit(33, 190, BLACK, LIME, &gfx);
+  dec = new LedDigit(65, 190, BLACK, LIME, &gfx);
+  uni = new LedDigit(97, 190, BLACK, LIME, &gfx);
+
+}
+
+void loop() {
+  mill->update(m);
+  cent->update(c);
+  dec->update(d);
+  uni->update(u);
+  int touchXpos,touchYpos;
+  btnP = false;
+  if (gfx.touch_Update()) { // Update touch parameter values
+    touchXpos = gfx.touch_GetX();
+    touchYpos = gfx.touch_GetY();
+    if(touchYpos >= sldy && touchYpos <= sldy + sldh){
+      sldr->touched(touchXpos, touchYpos);
+    } else if(touchYpos <= btnh){
+      btn->touched(touchXpos, touchYpos);
+      btnP = true;
+    } else if(touchXpos >= 0 && touchXpos <= 120 && touchYpos >= sld2y && touchYpos <= sld2y + sld2h){
+    sldrtwo->touched(touchXpos, touchYpos);
+  }
+  if(!btnP){
+    btn->released();
+  }
+ }
+ u++;
+ if(u == 10){
+  u = 0;
+  d++;
+  if(d == 10){
+    d = 0;
+    c++;
+    if(c == 10){
+      c = 0;
+      m++;
+      if(m == 10){
+        m = 0;
+      }
+    }
+  }
+ }
+ delay(20);
+ yield();
+
+}
