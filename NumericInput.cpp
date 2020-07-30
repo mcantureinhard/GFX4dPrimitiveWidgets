524c99ccdffdd4c1b8c8f2a79e8931fa8f90ad2a
Manuel Cantu <m.cantu.reinhard@gmail.com>
2020-07-30 13:45:13 +0200

    Add flags to remove decimal point or negative sign from numeric input

--- NumericInput.cpp
+++ NumericInput.cpp
@@ -1,10 +1,11 @@
 #include "NumericInput.h"
 
-NumericInput::NumericInput(uint16_t colorb, uint16_t colorbp, uint16_t tcolor, uint16_t x, uint16_t y, uint16_t w, uint16_t h, GFX4d *gfx, void (*callback)(int, int), int textsize, int id)
+NumericInput::NumericInput(uint16_t colorb, uint16_t colorbp, uint16_t tcolor, uint16_t x, uint16_t y, uint16_t w, uint16_t h, GFX4d *gfx, void (*callback)(int, int), int textsize, int id, uint8_t flags)
 : PrimitiveInput{colorb, colorbp, tcolor, x, y, w, h, gfx, callback, id}
 {
     this->bw = w/3;
     this->bh = h/4;
+    this->flags = flags;
     buttons = new PWButton*[12]; // 0 - 11
     for(int i = 0; i < 4; i++){
         for(int j = 0; j < 3; j++){
@@ -16,7 +17,8 @@ NumericInput::NumericInput(uint16_t colorb, uint16_t colorbp, uint16_t tcolor, u
             } else if(id == 12){
                 id = -2;
             }
-            buttons[i*3+j] = new PWButton(colorb, colorbp, tcolor, x + this->bw*j, y + this->bh*i, this->bw, this->bh, numbers[i*3+j], gfx, callback, textsize, id);
+            bool disabled = ((((i*3+j) == NEG_POS) && ((flags & NEG_FLAG) == 0)) || (((i*3+j) == DEC_POS) && ((flags & DEC_FLAG) == 0)));
+            buttons[i*3+j] = new PWButton(colorb, colorbp, tcolor, x + this->bw*j, y + this->bh*i, this->bw, this->bh, disabled ? "" : numberStrings[i*3+j], gfx, callback, textsize, id);
         }
     }
 }
@@ -29,7 +31,7 @@ NumericInput::~NumericInput(){
 }
 
 void NumericInput::touched(uint16_t x, uint16_t y){
-    // Won't check for bountries
+    // Won't check for boundaries
     int i = (x - this->x)/this->bw;
     int j = (y - this->y)/this->bh;
     int touch = j*3+i;
@@ -37,8 +39,11 @@ void NumericInput::touched(uint16_t x, uint16_t y){
         blockNext = true;
         buttons[lastTouched]->released();
     }
-    buttons[touch]->touched(x,y);
-    lastTouched = touch;
+    bool disabled = (((touch == NEG_POS) && ((flags & NEG_FLAG) == 0)) || ((touch == DEC_POS) && ((flags & DEC_FLAG) == 0)));
+    if(!disabled){
+        buttons[touch]->touched(x,y);
+        lastTouched = touch;
+    }
 }
 
 void NumericInput::released(){
--- NumericInput.h
+++ NumericInput.h
@@ -4,7 +4,13 @@
 #include "PrimitiveInput.h"
 #include "PWButton.h"
 
-String numbers[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "-", "0", "."};
+#define DEC_FLAG 1
+#define NEG_FLAG 2
+
+#define DEC_POS 11
+#define NEG_POS 9
+
+const String numberStrings[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "-", "0", "."};
 
 class NumericInput: public PrimitiveInput {
     private:
@@ -13,8 +19,9 @@ class NumericInput: public PrimitiveInput {
     uint16_t bh;
     int lastTouched = -1;
     bool blockNext = false;
+    uint8_t flags;
     public:
-    NumericInput(uint16_t colorb, uint16_t colorbp, uint16_t tcolor, uint16_t x, uint16_t y, uint16_t w, uint16_t h, GFX4d *gfx, void (*callback)(int, int), int textsize, int id);
+    NumericInput(uint16_t colorb, uint16_t colorbp, uint16_t tcolor, uint16_t x, uint16_t y, uint16_t w, uint16_t h, GFX4d *gfx, void (*callback)(int, int), int textsize, int id, uint8_t flags);
     ~NumericInput();
     void touched(uint16_t x, uint16_t y);
     void released();
