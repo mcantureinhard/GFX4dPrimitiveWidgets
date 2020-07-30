d2536021c726ecae63c7272dadf0fa64accac705
Manuel Cantu <m.cantu.reinhard@gmail.com>
2020-07-30 11:32:42 +0200

    Not using ref for button string

--- ToggleButton.cpp
+++ ToggleButton.cpp
@@ -1,7 +1,7 @@
 #include "ToggleButton.h"
 
 ToggleButton::ToggleButton(uint16_t *colorb, int colors, uint16_t colorbp, uint16_t tcolor, uint16_t x, uint16_t y, uint16_t w, uint16_t h, String *textOptions, int options, GFX4d *gfx, void (*callback)(int, int), int textsize, int id)
-: PWButton{colorb[0], colorbp, tcolor, x, y, w, h, &textOptions[0], gfx, callback, textsize, id}
+: PWButton{colorb[0], colorbp, tcolor, x, y, w, h, textOptions[0], gfx, callback, textsize, id}
 {
     this->textOptions = textOptions;
     this->options = options;
@@ -24,7 +24,7 @@ void ToggleButton::touched(uint16_t x, uint16_t y){
         selected = (selected + 1) % options;
         selectedColor = (selectedColor + 1) % colors;
         colorb = colorsb[selectedColor];
-        updateText(&textOptions[selected]);
+        updateText(textOptions[selected]);
         drawButton();
     }
 }
